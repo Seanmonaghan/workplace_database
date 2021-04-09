eb54e33634ca82eb5568f07504b7bea4e74cb9bf
Sean Monaghan <smonagha@conncoll.edu>
2021-04-09 15:30:25 -0400

    updated schema

---
+++ schema/managementSchema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE cms;
+
+CREATE TABLE department(
+id INTEGER NOT NULL,
+name VARCHAR(30),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+id INTEGER NOT NULL,
+title VARCHAR(30),
+salary DECIMAL(10,2),
+department_ID INTEGER,
+PRIMARY KEY (id),
+FOREIGN KEY (department_ID) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+id INTEGER NOT NULL,
+first_name VARCHAR(30),
+last_name VARCAHR(30),
+role_id INTEGER NOT NULL,
+manager_id INTEGER,
+PRIMARY KEY(id),
+FOREIGN KEY(role_id) REFERENCES role(id),
+FOREIGN KEY(manager_id) REFERENCES manager
+);

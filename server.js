b0d7c80a981c6a7bfd7d770ce75de2ca962265ec
Sean Monaghan <smonagha@conncoll.edu>
2021-04-10 19:44:04 -0400

    resolved the addDepartment and addRole functions. Working on addEmployee (join all three)

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const connection = mysql.createConnection({
 // View Functions
 
 const viewDepartments = () => {
-    connection.query('SELECT * FROM department', (err, results) => {
-      if (err) throw err;
-      const table = cTable.getTable(results)
-      console.log(table);
-      start();
-    })
+  connection.query('SELECT * FROM department', (err, results) => {
+    if (err) throw err;
+    const table = cTable.getTable(results)
+    console.log(table);
+    start();
+  })
 };
 
 const viewRoles = () => {
@@ -37,85 +37,146 @@ const viewRoles = () => {
 
 const viewEmployees = () => {
   connection.query('SELECT employee.employee_id, employee.first_name, employee.last_name, employee.manager_id, employee.role_id, role.salary, role.title, role.role_id FROM employee INNER JOIN role ON employee.role_id = role.role_id;',
-  (err, results) => {
-    if (err) throw err;
-    const table = cTable.getTable(results)
-    console.log(table);
-    start();
-  })
+    (err, results) => {
+      if (err) throw err;
+      const table = cTable.getTable(results)
+      console.log(table);
+      start();
+    })
 };
 
 // Add Functions
 
 const addDepartment = () => {
   inquirer
-  .prompt({
-    name: 'newDepartment',
-    type: "input",
-    question: "What is the name of the department you want to add?"
-  })
-  .then((answer) => {
-    connection.query(
-      'INSERT INTO department SET ?',
-      {
-        name: answer.newDepartment
-      },
-      (err) => {
-        if (err) throw err;
-        console.log("Your department has been added.")
-        start();
-      }
-    )
-  }) 
+    .prompt({
+      name: 'newDepartment',
+      type: "input",
+      question: "What is the name of the department you want to add?"
+    })
+    .then((answer) => {
+      connection.query(
+        'INSERT INTO department SET ?', {
+          name: answer.newDepartment
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("Your department has been added.")
+          start();
+        }
+      )
+    })
 }
 
 const addRole = () => {
-  connection.query('SELECT * FROM department', (err, results) => {
+  connection.query('SELECT * FROM role', (err, results) => {
     if (err) throw err;
     inquirer
-    .prompt([
-      {
-        name: 'newRole',
-        type: "input",
-        message: "What is the name of the role you want to add?"
-      }, 
-      {
-        name: 'salary',
-        type: 'input',
-        message: "What is the salary of this role?"
-      },
-      {
-        name: 'choice',
-        type: 'rawlist',
-        choices() {
-          const choicesArray = [];
-          results.forEach(({ name }) => {
-            choicesArray.push(name)
-          });
-          return choicesArray;
+      .prompt([{
+          name: 'newRole',
+          type: "input",
+          message: "What is the name of the role you want to add?"
         },
-        message: "What is the department ID that this role is a part of?"
-      }
-    ])
-    .then((answer) => {
-      // console.log(answer);
-      let chosenDepartment;
-      results.forEach((department) => {
-        if (department.name === answer.choice) {
-          chosenDepartment = department.department_id;
+        {
+          name: 'salary',
+          type: 'input',
+          message: "What is the salary of this role?"
+        },
+        {
+          name: 'choice',
+          type: 'rawlist',
+          choices() {
+            const choicesArray = [];
+            results.forEach(({
+              name
+            }) => {
+              choicesArray.push(name)
+            });
+            return choicesArray;
+          },
+          message: "What is the department ID that this role is a part of?"
         }
-        ;
+      ])
+      .then((answer) => {
+        // console.log(answer);
+        let chosenDepartment;
+        results.forEach((department) => {
+          if (department.name === answer.choice) {
+            chosenDepartment = department.department_id;
+          };
+        })
+        
+        connection.query(
+          'INSERT INTO role SET ?', 
+          {
+            'title': answer.newRole,
+            'salary': answer.salary,
+            'department_id': chosenDepartment
+          },
+          (err) => {
+            if (err) throw err;
+            console.log("Your role was created successfully")
+            start();
+          }
+        )
       })
-      console.log(chosenDepartment)
-      start();
   })
-  }
-) 
 }
 
-const addEmployee = () => {
-
-}
+// const addEmployee = () => {
+//   connection.query('SELECT * FROM role', (err, results) => {
+//     if (err) throw err;
+//     inquirer
+//       .prompt([{
+//           name: 'firstName',
+//           type: "input",
+//           message: "What is the first name of the employee"
+//         },
+//         {
+//           name: 'lastName',
+//           type: "input",
+//           message: "What is the last name of the employee"
+//         },
+//         {
+//           name: 'roleChoice',
+//           type: 'rawlist',
+//           choices() {
+//             const choicesArray = [];
+//             results.forEach(({
+//               name
+//             }) => {
+//               choicesArray.push(name)
+//             });
+//             return choicesArray;
+//           },
+//           message: "What is the department ID that this role is a part of?"
+//         }
+//       ])
+//       .then((answer) => {
+//         // console.log(answer);
+//         let chosenDepartment;
+//         results.forEach((department) => {
+//           if (department.name === answer.choice) {
+//             chosenDepartment = department.department_id;
+//           };
+//         })
+        
+//         connection.query(
+//           'INSERT INTO role SET ?', 
+//           {
+//             'title': answer.newRole,
+//             'salary': answer.salary,
+//             'department_id': chosenDepartment
+//           },
+//           (err) => {
+//             if (err) throw err;
+//             console.log("Your role was created successfully")
+//             start();
+//           }
+//         )
+//       })
+//   })
+// }
 
 // Update Functions
 
@@ -134,59 +195,58 @@ const updateManager = () => {
 
 
 const start = () => {
-    inquirer
-      .prompt({
-        name: "firstChoice",
-        type: 'list',
-        message: "What would you like to do?",
-        choices: [
-          'View all departments',
-          'View all roles',
-          'View all employees',
-          'Add a department',
-          'Add a role',
-          'Add an employee',
-          "Update an employee's role",
-          "Update an employee's manager",
-          'Delete a department',
-          'Delete a role',
-          'Delete an employee',
-          'View a total utilized budget of a department'
-        ]
-      })
-      .then((answer) => {
-        if (answer.firstChoice === 'Add a department') {
-          addDepartment();
-        } else if (answer.firstChoice === 'View all departments') {
-          viewDepartments();
-        } else if (answer.firstChoice === 'View all roles') {
-          viewRoles();
-        } else if (answer.firstChoice === 'View all employees') {
-          viewEmployees();
-        } else if (answer.firstChoice === 'Add a role') {
-          addRole();
-        } else if (answer.firstChoice === 'Add an employee') {
-          addEmployee();
-        } else if (answer.firstChoice === "Update an employee's role") {
-          updateRole();
-        } else if (answer.firstChoice === "Update an employee's manager") {
-          updateManager();
-        } else if (answer.firstChoice === "Delete a department") {
-          updateManager();
-        } else if (answer.firstChoice === "Delete a role") {
-          updateManager();
-        } else if (answer.firstChoice === "Update an employee's manager") {
-          updateManager();
-        } 
-          
-      })
+  inquirer
+    .prompt({
+      name: "firstChoice",
+      type: 'list',
+      message: "What would you like to do?",
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        "Update an employee's role",
+        "Update an employee's manager",
+        'Delete a department',
+        'Delete a role',
+        'Delete an employee',
+        'View a total utilized budget of a department'
+      ]
+    })
+    .then((answer) => {
+      if (answer.firstChoice === 'Add a department') {
+        addDepartment();
+      } else if (answer.firstChoice === 'View all departments') {
+        viewDepartments();
+      } else if (answer.firstChoice === 'View all roles') {
+        viewRoles();
+      } else if (answer.firstChoice === 'View all employees') {
+        viewEmployees();
+      } else if (answer.firstChoice === 'Add a role') {
+        addRole();
+      } else if (answer.firstChoice === 'Add an employee') {
+        addEmployee();
+      } else if (answer.firstChoice === "Update an employee's role") {
+        updateRole();
+      } else if (answer.firstChoice === "Update an employee's manager") {
+        updateManager();
+      } else if (answer.firstChoice === "Delete a department") {
+        deleteDepartment();
+      } else if (answer.firstChoice === "Delete a role") {
+        deleteRole();
+      } else if (answer.firstChoice === "Delete an employee") {
+        deleteEmployee();
+      }
+    })
 };
 
 
 // connect to the mysql server and sql database
 connection.connect((err) => {
-    if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    
-    start();
-  });
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+
+  start();
+});

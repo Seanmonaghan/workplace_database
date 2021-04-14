8999f3c1ae6367126218181f264a13f64196bdae
Sean Monaghan <smonagha@conncoll.edu>
2021-04-14 13:47:43 -0400

    cleaned up code to make it more presentable for example

--- schema/managmenetSeed.sql
+++ schema/managmenetSeed.sql
@@ -7,4 +7,4 @@ INSERT INTO role(title, salary, department_id)
 VALUES ('CCA', 50000, 3), ('RCA', 56000, 3), ('Clerk', 48000, 4), ('Supevisor', 65000, 2), ('Labor Relations', 60000, 1);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ('Bob', 'Smith', 3, 5), ('Susan', 'Strong', 1, null), ('Wiley', 'Coyote', 5, 4), ('Fred', 'Flintstone', 2, null), ('Jack', 'Daniels', 4, 2);
+VALUES ('David', 'Bowie', 3, 5), ('Susan', 'Strong', 1, null), ('Wiley', 'Coyote', 5, 4), ('Fred', 'Flintstone', 2, null), ('Jack', 'Daniels', 4, 2);

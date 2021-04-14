80b0484ff593a15999b3f528d1e6eb479fe4f7a2
Sean Monaghan <smonagha@conncoll.edu>
2021-04-14 15:09:52 -0400

    updated Seed file and completed Readme with video overview of the site

--- schema/managmenetSeed.sql
+++ schema/managmenetSeed.sql
@@ -6,5 +6,5 @@ VALUES ('HR'), ('Management'), ('Carriers'), ('Clerks');
 INSERT INTO role(title, salary, department_id)
 VALUES ('CCA', 50000, 3), ('RCA', 56000, 3), ('Clerk', 48000, 4), ('Supevisor', 65000, 2), ('Labor Relations', 60000, 1);
 
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ('David', 'Bowie', 3, 5), ('Susan', 'Strong', 1, null), ('Wiley', 'Coyote', 5, 4), ('Fred', 'Flintstone', 2, null), ('Jack', 'Daniels', 4, 2);
+INSERT INTO employee(first_name, last_name, role_id, manager_id, manager_name)
+VALUES ('David', 'Bowie', 3, 5, 'Jack'), ('Susan', 'Strong', 1, null, null), ('Wiley', 'Coyote', 5, 4, 'Fred'), ('Fred', 'Flintstone', 2, null, null), ('Jack', 'Skellington', 4, 2, 'Susan');
